9a5b2309ea6a57c732786696b02007b145963250
cristianovilasboas <cnvboas@gmail.com>
2019-08-22 09:17:29 -0300

    Add files via upload

---
+++ tnsping.py
@@ -0,0 +1,18 @@
+import subprocess
+import re
+
+def tnsping(base):
+    try:
+        command_string = 'tnsping {b}'.format(b=base)
+        proc = subprocess.Popen(command_string, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+        stdout, stderr = proc.communicate()
+        dns = str(stdout)
+        dns = re.search('\([D].+[\)]{3}', dns).group(0)
+        return dns
+    except:
+        print('BASE NAO ENCONTRADA')
+    
+    
+
+if __name__ == '__main__':
+    print(tnsping('ALIAS'))
